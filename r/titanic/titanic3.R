76759e9c8b20a107cf52b68120a36048a763f5fb
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-06-13 21:36:15 +1000

    3

--- r/titanic/titanic3.R
+++ r/titanic/titanic3.R
@@ -5,6 +5,10 @@ data <- read.csv("titanic_train.csv")
 
 dev <- round(sd(data$Fare), digits = 2)
 med <- round(median(data$Fare), digits = 2)
-cm <- c(dev, med)
+cm <- data.frame(c(dev, med), row.names = c("deviation", "median"))
+pd <- c(dev, med)
 
-qplot(c("dev", "med"), cm)
+qplot(x=rownames(cm), y=pd, geom="col", fill=pd, main ="Fare median and deviation", xlab = "")
+
+med 
+dev
